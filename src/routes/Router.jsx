9545186103c552cf2051b8c5ed3281d8b9cf104e
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-06-14 22:25:26 +0600

    add authentication router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,26 +2,41 @@ import { createBrowserRouter } from "react-router";
 
 import HomeLayout from "../layouts/HomeLayout";
 import Hero from "../home/Hero";
-
+import Login from "../components/Login";
+import AuthenticationLayout from "../layouts/AuthenticationLayout";
+import Signup from "../components/Signup";
 
 const Router = createBrowserRouter([
     {
-        path: '/',
+        path: "/",
         Component: HomeLayout,
         children: [
             {
                 index: true,
                 loader: () => fetch("/skills.json"),
-                Component: Hero
+                Component: Hero,
             },
-
-        ]
-
+        ],
     },
     {
-        path: '/about',
-        element: <h2>About</h2>
-    }
-])
+        path: '/',
+        Component: AuthenticationLayout,
+        children: [
+            {
+                path: '/login',
+                Component: Login
+            },
+            {
+                path: '/signup',
+                Component: Signup
+            }
+
+        ]
+    },
+    {
+        path: "*",
+        Component: () => <h1>404 - Page Not Found</h1>,
+    },
+]);
 
 export default Router;
