dc834b81c431f688b803fd3402152f4f5185100c
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-06-14 12:12:26 +0600

    Home layout is done

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,15 +1,23 @@
 import { Outlet, useLoaderData } from 'react-router';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 
 const HomeLayout = () => {
     const data = useLoaderData();
     console.log(data);
     return (
-        <div className='w-11/12 mx-auto'>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
+        <>
+            <nav>
+                <Navbar></Navbar>
+            </nav>
+            <div className='w-11/12 mx-auto'>
+                <Outlet></Outlet>
+            </div>
+            <footer>
+                <Footer></Footer>
+            </footer>
+        </>
     );
 };
 
