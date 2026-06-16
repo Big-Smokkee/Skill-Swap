4279f1c69e09f452798c67d10c5f7a7efeaafe24
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-06-16 14:03:12 +0600

    toaster implementation

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import Router from './Routes/Router.jsx'
+import AuthProvider from './provider/AuthProvider.jsx'
+import { Bounce, ToastContainer } from 'react-toastify'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Router}>
-
-    </RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Router}></RouterProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </AuthProvider>
   </StrictMode>,
 )
