20b77bd2fb0e595d125f8dfd81907c4a59d67d1d
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-06-18 22:37:17 +0600

    commented the consoles

--- src/pages/SkillDetails.jsx
+++ src/pages/SkillDetails.jsx
@@ -2,29 +2,18 @@ import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
 import SkillDetailsCard from "../components/SkillDetailsCard";
 
-// {
-//     "skillId": 1,
-//  "skillName": "Beginner Guitar Lessons",
-//  "providerName": "Alex Martin",
-//  "providerEmail": "alex@skillswap.com",
-//  "price": 20,
-// "rating": 4.8,
-// "slotsAvailable": 3,
-// "description": "Acoustic guitar classes for complete beginners. Learn chords, strumming, and basic songs.",
-// "image": "https://i.ibb.co.com/tpXfyjvD/vitaly-gariev-MPh-Xl-Kd-T5-Vo-unsplash.jpg",
-// "category": "Music"
-// }
 
 const SkillDetails = () => {
     const { id } = useParams();
     const data = useLoaderData();
     const [skill, setSkill] = useState({});
+
     useEffect(() => {
         // console.log(id, skill);
         const skill = data.find(d => d.skillId == id)
         setSkill(skill);
     }, [data, id])
-    console.log(id, skill);
+    // console.log(id, skill);
     return (
         <section>
             <SkillDetailsCard skill={skill}></SkillDetailsCard>
