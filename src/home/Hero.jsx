96440f24dd0083428330903d5a5a182beead8fcd
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-06-19 12:47:58 +0600

    feat: add responsiveness to the hero section

--- src/home/Hero.jsx
+++ src/home/Hero.jsx
@@ -17,9 +17,9 @@ const Hero = () => {
 
         <div className="my-10 px-4">
             <div className="text-center">
-                <h2 className="text-4xl  bg-linear-to-r from-emerald-400 to-emerald-600 
+                <h2 className="text-2xl md:text-4xl  bg-linear-to-r from-emerald-400 to-emerald-600 
     bg-clip-text text-transparent font-bold">Swap Skills</h2>
-                <h2 className="text-5xl mt-4">Unlock Potential !</h2>
+                <h2 className="text-3xl md:text-5xl mt-4">Unlock Potential !</h2>
             </div>
             <section className='my-10'>
                 <Swiper
@@ -125,7 +125,7 @@ const Hero = () => {
                     </SwiperSlide>
                 </Swiper>
             </section>
-            <p className='text-center text-3xl my-8 px-40'>``Connect with people worldwide to teach what you know and learn what you don't. All for free.``</p>
+            <p className='text-center text-3xl my-8 px-6 md:px-20 lg:px-40'>``Connect with people worldwide to teach what you know and learn what you don't. All for free.``</p>
 
             <div className='flex items-center justify-center'>
                 <button className="btn bg-emerald-600 text-white hover:bg-black hover:text-emerald-400 text-lg px-8 py-4">
